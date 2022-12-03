1930ce1ab16a4b654f99277a01234b189a1aac48
Qdesai <qaasimdesai786@gmail.com>
2022-12-02 22:52:05 -0500

    installed the react router dom and added a login page  STARTING ASSIGNMENT 4

--- assignment3/src/index.js
+++ assignment3/src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import Data from './Components/Data';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+ 
   <React.StrictMode>
+     <BrowserRouter>
     <App />
     <Data/>
-  </React.StrictMode>
+    </BrowserRouter>
+    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
